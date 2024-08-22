6095f605b771f02993d2dcf6f01dfce0c1558811
Igor Dubinin <i.dubinin@x5.ru>
2024-08-22 16:09:27 +0400

    leetcode

--- leetcode/hard/664_strangePrinter.py
+++ leetcode/hard/664_strangePrinter.py
@@ -2,8 +2,31 @@ from typing import List
 
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        letters = set(s)
-        return len(letters)
+        letters = []
+        for i in range(len(s)):
+            if i+1 == len(s) or s[i] != s[i+1]:
+                letters.append(s[i])
+
+        cache = {}
+        def rec(start, end):
+            if start > end:
+                return 0
+
+            if (start, end) in cache:
+                return cache[(start, end)]
+
+            prints = 1 + rec(start + 1, end)
+
+            for i in range(start + 1, end + 1):
+                if s[i] == s[start]:
+                    parts = rec(start, i - 1) + rec(i + 1, end)
+                    prints = min(parts, prints)
+
+
+            cache[(start, end)] = prints
+            return prints
+
+        return rec(0, len(s) - 1)
 
 
 
