ba4065ffab641064154f44b2ec44c75c745dcc37
Igor Dubinin <i.dubinin@x5.ru>
2024-11-19 14:43:38 +0400

    leetcode

--- leetcode/medium/2461_maximumSubarraySum.js
+++ leetcode/medium/2461_maximumSubarraySum.js
@@ -3,7 +3,7 @@
  * @param {number} k
  * @return {number}
  */
-var maximumSubarraySum = function(nums, k) {
+var maximumSubarraySum_1 = function(nums, k) {
     const visited = new Map();
     let sum = 0;
     let res = 0;
@@ -39,6 +39,57 @@ var maximumSubarraySum = function(nums, k) {
     return res;
 };
 
+/*
+
+1, 2, 3, 3, 3, 4, 5, 6
+6
+12
+15
+*/
+
+var maximumSubarraySum = function(nums, k) {
+    const visitedIds = new Map();
+    let sum = 0;
+    let res = 0;
+
+    let start = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (k === 1) {
+            res = Math.max(res, nums[i]);
+            continue;
+        }
+
+        sum += nums[i];
+        const visitedId = visitedIds.get(nums[i]) === undefined ? -1 : visitedIds.get(nums[i]);
+
+        if (visitedId !== -1) {
+            for (let j = start; j <= visitedId; ++j) {
+                sum -= nums[j];
+                visitedIds.delete(nums[j]);
+            }
+            start = visitedId + 1;
+
+            if (start === i) {
+                visitedIds.set(nums[i], i);
+                continue;
+            };
+        }
+        visitedIds.set(nums[i], i);
+
+        if (i - start === k) {
+            sum -= nums[start];
+            visitedIds.delete(nums[start]);
+            start += 1;
+        }
+
+        if (visitedIds.size === k) {
+            res = Math.max(res, sum);
+        }
+    }
+
+    return res;
+};
+
 
 const test = () => {
     const params = [
@@ -54,7 +105,20 @@ const test = () => {
             input: [[9,9,9,1,2,3], 3],
             output: 12,
         },
+        {
+            input: [[1, 2, 3, 3, 3, 4, 5, 6], 3],
+            output: 15,
+        },
+        {
+            input: [[1, 2, 3, 4, 5, 6], 1],
+            output: 6,
+        },
+        {
+            input: [[1,1,2], 2],
+            output: 3,
+        },
     ];
+
 
     for (const { input, output } of params) {
         const result = maximumSubarraySum(...input);
--- leetcode/medium/714_maxProfit.js
+++ leetcode/medium/714_maxProfit.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 var maxProfit = function(prices, fee) {
+    const profit = new Array(prices.length).fill(0);
+    const buy = new Array(prices.length).fill(0);
 
+    buy[0] = prices[0];
+
+    for (let i = 1; i < prices.length; ++i) {
+        profit[i] = Math.max(profit[i - 1], prices[i] - buy[i - 1] - fee); // my income, maximisation
+        buy[i] = Math.min(buy[i - 1], prices[i] - profit[i]);  // my expenses, minimisation
+    }
+
+    return profit[profit.length - 1];
 };
 
 
@@ -20,17 +30,11 @@ var maxProfit = function(prices, fee) {
 
 2
     1   3   2   8   4   9
-1  -2   0  -1   5   1   6
-3  -2  -2  -3   3  -1   4
-2
-8
-4
-9
+s   0   0   0   5   5 9--1-2 = 8
+b   1   1   1   1  -1  1
 
-
-
-
-
+sell[i] = max(sell[i-1], price[i] - buy[i - 1])
+buy[i] = min(buy[i - 1], price[i]- sell[i])
 
 
 
@@ -49,7 +53,7 @@ const test = () => {
     ];
 
     params.forEach(({input, output}) => {
-        const result = shortestSubarray(...input);
+        const result = maxProfit(...input);
 
         console.log(
             JSON.stringify(result) === JSON.stringify(output) ? 'SUCCESS ' : 'ERROR ',
