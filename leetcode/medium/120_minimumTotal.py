6ec420bff652c8a4d7cd53ce2fee52fc85afc0bf
Igor Dubinin <i.dubinin@x5.ru>
2025-01-22 18:50:28 +0400

    leetcode

--- leetcode/medium/120_minimumTotal.py
+++ leetcode/medium/120_minimumTotal.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(1, len(triangle)):
             arr.append([0] * (i + 1))
             for j in range(i + 1):
-                left = float('inf')
+                left = right = float('inf')
                 if j - 1 >= 0:
                     left = arr[i-1][j-1]
                 if j < i:
