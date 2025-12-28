ca57a07e1623d9c4232eeff3afe98cb8ba8a8fc3
idubinin <idubininmail@gmail.com>
2025-12-28 17:51:26 +0400

    leetcode

--- leetcode/medium/310_findMinHeightTrees.py
+++ leetcode/medium/310_findMinHeightTrees.py
@@ -1,43 +1,38 @@
 from typing import List
 import json
 import heapq
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        adj = [[] for i in range(n)]
+        if n == 1:
+            return [0]
+
+        adj = [set() for i in range(n)]
         for s, e in edges:
-            adj[s].append(e)
-            adj[e].append(s)
+            adj[s].add(e)
+            adj[e].add(s)
 
-        val = float('inf')
-        res = []
+        nodesQueue = deque()
 
-        def dfs(node, parent, path):
-            nonlocal val, res, adj
+        for i in range(n):
+            if len(adj[i]) == 1:
+                nodesQueue.append((i, 0))
 
-            localPath = 0
+        rows = []
+
+        while nodesQueue:
+            node, length = nodesQueue.popleft()
+            if len(rows) == length:
+                rows.append([])
+            rows[length].append(node)
+
             for nei in adj[node]:
-                if nei == parent:
-                    continue
+                adj[nei].remove(node)
+                if len(adj[nei]) == 1:
+                    nodesQueue.append((nei, length+1))
 
-                r = dfs(nei, node, path + 1)
-                localPath = max(localPath, r)
-
-            height = max(path, localPath)
-
-            if height == val:
-                res.append(node)
-            elif height < val:
-                val = height
-                res = [node]
-
-            return localPath + 1
-
-        dfs(0, 0, 0)
-
-
-        return res
+        return rows[-1]
 
 
 
@@ -52,6 +47,24 @@ def test ():
         {
             'input': [6, [[3,0],[3,1],[3,2],[3,4],[5,4]]],
             'output': [3,4],
+        },
+        {
+            'input': [2, [[0,1], [1,0]]],
+            'output': [0,1],
+        },
+        {
+            'input': [
+                4,
+                [[0,1], [1,2], [2, 3]]
+            ],
+            'output': [1,2],
+        },
+        {
+            'input': [
+                5,
+                [[0,1], [1,2], [2, 3], [3,4]]
+            ],
+            'output': [2],
         },
     ]
     solution = Solution()
