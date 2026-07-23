10c99b9a8ac3e52e04247977f1d3dfb49e1ec42b
Igor Dubinin <i.dubinin@x5.ru>
2026-07-23 13:26:29 +0300

    leetcode

--- leetcode/medium/1288_removeCoveredIntervals.py
+++ leetcode/medium/1288_removeCoveredIntervals.py
@@ -1,8 +1,8 @@
-from typing import List
-import json
-from collections import deque, defaultdict
 import heapq
+import json
+from collections import defaultdict, deque
 from functools import lru_cache
+from typing import List
 
 
 class Solution:
@@ -10,47 +10,44 @@ class Solution:
         intervals.sort()
 
         stack = []
-
         for s, e in intervals:
-            while stack and s <= stack[-1][0] and e >= stack[-1][0]:
+            while stack and s <= stack[-1][0] and e >= stack[-1][1]:
                 stack.pop()
 
-            if not stack:
-                stack.append((s, e))
+            if stack and stack[-1][0] <= s and stack[-1][1] >= e:
                 continue
-
-            if not (stack[-1][0] <= s and stack[-1][1] >= e):
-                stack.append((s, e))
+            stack.append((s, e))
 
         return len(stack)
 
-def test ():
+
+def test():
     params = [
         {
-            'input': [[1,4],[3,6],[2,8]],
-            'output': 2,
+            "input": [[1, 4], [3, 6], [2, 8]],
+            "output": 2,
         },
         {
-            'input': [[1,4],[2,3]],
-            'output': 1,
+            "input": [[1, 4], [2, 3]],
+            "output": 1,
         },
     ]
     solution = Solution()
 
     for param in params:
-        intervals = param['input']
+        intervals = param["input"]
         result = solution.removeCoveredIntervals(intervals)
-        correct = json.dumps(result) == json.dumps(param['output'])
+        correct = json.dumps(result) == json.dumps(param["output"])
 
-        msg = 'SUCCESS' if correct else 'ERROR'
-        msg += '\n'
+        msg = "SUCCESS" if correct else "ERROR"
+        msg += "\n"
         if not correct:
-            msg += 'input ' + json.dumps(param['input']) + '\n'
-            msg += 'output ' + json.dumps(param['output']) + '\n'
-            msg += 'result ' + json.dumps(result) + '\n'
+            msg += "input " + json.dumps(param["input"]) + "\n"
+            msg += "output " + json.dumps(param["output"]) + "\n"
+            msg += "result " + json.dumps(result) + "\n"
 
         print(msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
